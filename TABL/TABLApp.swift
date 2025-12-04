0af128bad22ae7935f6f40b7a5305adb4ab51ab8
Ignacio <ignaciocantoisado@gmail.com>
2025-12-04 13:22:28 -0500

    Initial Commit

---
+++ TABL/TABLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TABLApp.swift
+//  TABL
+//
+//  Created by Ignacio Canto on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct TABLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
